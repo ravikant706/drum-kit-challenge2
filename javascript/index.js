ba21ca9c49100df5455fae1ab611e76cc4851a76
ravikant706 <ravikant706@gmail.com>
2024-01-15 22:34:27 +0530

    press key time quick

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);    
     console.log(e);
+
     if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
 });
