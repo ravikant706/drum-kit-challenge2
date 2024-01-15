f26daba56e0f0e6d440e200ef93628b8885fb0d3
ravikant706 <ravikant706@gmail.com>
2024-01-15 23:40:24 +0530

    finally code write and adjust the code

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,12 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);    
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);    
-    console.log(e);
-
+    // console.log(e);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-
     key.classList.add('playing');
-    key.classList.toggle('palying');
-});
+};
 
 
 function removeTransition(e) {
@@ -17,5 +14,8 @@ function removeTransition(e) {
     this.classList.remove('playing');
 }
 
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
